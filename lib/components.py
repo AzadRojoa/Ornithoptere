cb0e8f343dabb758b611e073da4405cdcf3ae7ec
Laxenarde <alexandre.lebourch@ynov.com>
2025-08-06 15:25:12 +0200

    Add type hints to Joystick, Bouton, ServoMoteur, and Moteur classes for improved code clarity

--- lib/components.py
+++ lib/components.py
@@ -1,85 +1,85 @@
 from machine import Pin, ADC, PWM
+from typing import Tuple
 
 class Joystick:
-    def __init__(self, pin_x, pin_y, attenuation=ADC.ATTN_11DB):
-        self._x = ADC(Pin(pin_x))
+    def __init__(self, pin_x: int, pin_y: int, attenuation: int = ADC.ATTN_11DB) -> None:
+        self._x: ADC = ADC(Pin(pin_x))
         self._x.atten(attenuation)
-        self._y = ADC(Pin(pin_y))
+        self._y: ADC = ADC(Pin(pin_y))
         self._y.atten(attenuation)
 
-    def read(self):
+    def read(self) -> Tuple[int, int]:
         return (self._x.read(), self._y.read())
-    
+
     @property
-    def x(self):
+    def x(self) -> int:
         return self._x.read()
-    
+
     @property
-    def y(self):
+    def y(self) -> int:
         return self._y.read()
 
 class Bouton:
-    def __init__(self, pin, pull=Pin.PULL_UP):
-        self.pin = Pin(pin, Pin.IN, pull)
+    def __init__(self, pin: int, pull: int = Pin.PULL_UP) -> None:
+        self.pin: Pin = Pin(pin, Pin.IN, pull)
 
-    def is_pressed(self):
+    def is_pressed(self) -> bool:
         return self.pin.value() == 0
-    
+
     @property
-    def value(self):
+    def value(self) -> int:
         return self.pin.value()
 
 class ServoMoteur:
-    def __init__(self, pin, freq=50, min_us=500, max_us=2500):
-        self.pwm = PWM(Pin(pin), freq=freq)
-        self.min_us = min_us
-        self.max_us = max_us
-        self._angle = 0
+    def __init__(self, pin: int, freq: int = 50, min_us: int = 500, max_us: int = 2500) -> None:
+        self.pwm: PWM = PWM(Pin(pin), freq=freq)
+        self.min_us: int = min_us
+        self.max_us: int = max_us
+        self._angle: int = 0
 
-    def angle(self, deg):
+    def angle(self, deg: int) -> None:
         self._angle = deg
-        us = self.min_us + (self.max_us - self.min_us) * deg // 180
-        duty = int(us * 1023 // 20000)
+        us: int = self.min_us + (self.max_us - self.min_us) * deg // 180
+        duty: int = int(us * 1023 // 20000)
         self.pwm.duty(duty)
 
     @property
-    def frequency(self):
+    def frequency(self) -> int:
         return self.pwm.freq()
 
     @frequency.setter
-    def frequency(self, value):
+    def frequency(self, value: int) -> None:
         self.pwm.freq(value)
-    
+
     @property
-    def deg(self):
+    def deg(self) -> int:
         return self._angle
-    
-    @angle.setter
-    def deg(self, value):
+
+    @deg.setter
+    def deg(self, value: int) -> None:
         self.angle(value)
 
-
 class Moteur:
-    def __init__(self, pin, freq=1000):
-        self.pwm = PWM(Pin(pin), freq=freq)
-        self._duty = 512
+    def __init__(self, pin: int, freq: int = 1000) -> None:
+        self.pwm: PWM = PWM(Pin(pin), freq=freq)
+        self._duty: int = 512
 
-    def set_speed(self, value):
+    def set_speed(self, value: int) -> None:
         self._duty = int(value * 1023 / 100)
         self.pwm.duty(self._duty)
-    
+
     @property
-    def duty(self):
+    def duty(self) -> int:
         return self._duty
-    
+
     @duty.setter
-    def duty(self, value):
+    def duty(self, value: int) -> None:
         self.set_speed(value)
 
     @property
-    def frequency(self):
+    def frequency(self) -> int:
         return self.pwm.freq()
-    
+
     @frequency.setter
-    def frequency(self, value):
+    def frequency(self, value: int) -> None:
         self.pwm.freq(value)
